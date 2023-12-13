381df3b8ed464371e4c3e96cd8b5b98db31d20b2
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2023-12-13 23:44:43 +0300

    task for strings methods

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toUpperCase();
+
+  if (str.includes("XXX")) {
+    return true;
+  } else if (!str.includes("1xBet") & str.startsWith("1X")) {
+    return true;
+  } return false;
 }
+
+
+
