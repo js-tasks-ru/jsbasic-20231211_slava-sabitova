656e9f0bc314f686d19f6b5ff071bbb2714a5b25
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2023-12-30 21:57:36 +0300

    change table rows and table cells task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.rows;
+
+  for (let td of rows) {
+
+    let cell = td.querySelectorAll("td");
+
+    if (cell[3].hasAttribute("data-available")) {
+
+      if (cell[3].dataset.available === "true") {
+        td.classList.add("available");
+      } else {
+        td.classList.add("unavailable");
+      }
+
+    } else {
+      td.hidden = true;
+    }
+
+    if (cell[2].textContent === "m") {
+      td.classList.add("male");
+
+    } else if (cell[2].textContent === "f") {
+      td.classList.add("female");
+    }
+
+    if (cell[1].textContent < 18) {
+      td.style.textDecoration = "line-through";
+    }
+  }
 }
