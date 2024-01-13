af4631d91a4589c2605b1b11f350bf8f466b3a08
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-01-13 22:30:35 +0300

    table with deleted rows on click

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,37 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+
+    this.elem.innerHTML = `
+      <thead>
+          <tr>
+            <td>Имя</td>
+            <td>Возраст</td>
+            <td>Зарплата</td>
+            <td>Город</td>
+            <td></td>
+          </tr>
+      </thead>
+    `;
+
+    this.elem.insertAdjacentHTML('beforeend', rows.map(item => `<tr>
+        <td>${item.name}</td>
+        <td>${item.age}</td>
+        <td>${item.salary}</td>
+        <td>${item.city}</td>
+        <td><button>X</button></td>
+      </tr>
+    `).join(''));
+
+    console.log(this.elem);
+
+    this.elem.addEventListener('click', removeRow);
+
+    function removeRow(event) {
+      if (event.target.closest('button')) {
+        event.target.closest('tr').remove();
+      }
+    }
   }
 }
