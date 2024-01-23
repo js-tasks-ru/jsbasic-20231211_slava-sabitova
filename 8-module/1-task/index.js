751f5d9576dce796d4c714c0ca6cc0044b325848
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-01-23 20:52:13 +0300

    icon cart

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -7,12 +7,16 @@ export default class CartIcon {
     this.addEventListeners();
   }
 
+  // Отрисовать пустую иконку корзины - create empty cart icon
+
   render() {
     this.elem = createElement('<div class="cart-icon"></div>');
   }
 
+  // Заполнить её данными из объекта cart - fill the cart with data from the cart object
+
   update(cart) {
-    if (!cart.isEmpty()) {
+    if (!cart.isEmpty()) {      // возвращает true, если корзина пуста и false, если нет
       this.elem.classList.add('cart-icon_visible');
 
       this.elem.innerHTML = `
@@ -20,6 +24,10 @@ export default class CartIcon {
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
           <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
         </div>`;
+
+      // getTotalCount() {}  Возвращает общее количество товаров в корзине
+
+      // getTotalPrice() {}  Возвращает общую сумму всех товаров в корзине
 
       this.updatePosition();
 
@@ -38,7 +46,37 @@ export default class CartIcon {
     window.addEventListener('resize', () => this.updatePosition());
   }
 
+  // позиционировать иконку корзины на экране
   updatePosition() {
-    // ваш код ...
+    if (this.elem.offsetHeight !== 0 && document.body.clientWidth >= 767) {
+
+      if (document.body.getBoundingClientRect().top < 63) {
+
+        // отступ слева от корзины
+        let leftCartPosition = Math.min(
+
+          // расстояние от правого края container + 20px
+          document.querySelector('.container').getBoundingClientRect().right + 20,
+
+          // общая ширина страницы минус ширина иконки корзины и размер отступа от правого края (10px)
+          document.documentElement.clientWidth - this.elem.offsetWidth - 10
+        ) + 'px';
+
+        this.elem.style.position = "fixed";
+        this.elem.style.zIndex = "99999";
+        this.elem.style.right = "10px";              // отступ от правого края экрана
+        this.elem.style.left = leftCartPosition;
+
+      // вернуть как было, когда пользователь докрутил обратно до верха
+      } else {
+        Object.assign(this.elem.style, {
+          position: '',
+          top: '',
+          left: '',
+          zIndex: ''
+        });
+      }
+    }
+
   }
 }
