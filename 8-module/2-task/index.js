a2e713c104b87f8ea81c9cbf1a6201397148402c
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2024-01-24 15:28:56 +0300

    product list

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,54 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+
+    this.render();
+  }
+
+  render() {
+    this elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner">
+
+          <!--ВОТ ТУТ БУДУТ КАРТОЧКИ ТОВАРОВ-->
+
+        </div>
+      </div>
+    `);
+
+    this.createCards(this.products);
+  };
+
+  createCards(products) {
+    this.productsGridInner = this.elem.querySelector('.products-grid__inner');
+
+    for (let product of products) {
+      this.productsGridInner.append(new ProductCard(product).elem);
+    }
+  }
+
+  updateFilter(filters) {
+
+    Object.assign(this.filters, filters);
+
+    let products = this.products.filter(product => {
+
+      // если true, то показываем товары без орехов
+      if (this.filters.noNuts && product.nuts) return false;
+
+      //если true, то показываем вегетарианские товары
+      if (this.filters.vegeterianOnly && !product.vegeterian) return false;
+
+      // число от 0 до 4 – показывать только те товары, у которых в свойстве spiciness число меньше или равное заданному
+      if (this.filters.maxSpiciness < product.spiciness) return false;
+
+      // filters.category – уникальный идентификатор категории – показывать только те товары, у которых в свойстве category такое же значение
+      if (this.filters.category && product.category != this.filters.category) return false;
+
+      return true;
+    });
+
+    this.createCards(products);
+
   }
 }
